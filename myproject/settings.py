0da29dddda8c16765d9416aa2528e911907cd8b2
Debotos Das <debotosdas@gmail.com>
2019-01-14 07:07:29 +0600

    added panel title

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,6 +26,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    '127.0.0.1:3000'
+)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-Party Apps
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
 
@@ -57,6 +63,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Screenseal Admin"
+admin.site.site_title = "Screenseal Admin Portal"
+admin.site.index_title = "Welcome to Screenseal Admin Portal"
+
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls',)),
+    path('api/users/', include('users.urls', )),
 ]
