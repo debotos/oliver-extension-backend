c621ba09e9517f4b94a2fbcdb124d705f96192bc
Debotos Das <debotosdas@gmail.com>
2019-01-14 07:37:51 +0600

    removed extra url

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,6 +7,6 @@ admin.site.index_title = "Welcome to Screenseal Admin Portal"
 
 urlpatterns = [
     path('', admin.site.urls),
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls', )),
+    # path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
 ]
